8f2f05e0abcec519fd9960d1f0f2259ad52dd58a
ABalusov <abalusov@aladdin.ru>
2020-11-25 21:05:02 +0300

    added trace

--- cmd/channels/main.go
+++ cmd/channels/main.go
@@ -3,10 +3,29 @@ package main
 import (
 	"fmt"
 	"github.com/ArtDark/bgo_channels/pkg/card"
+	"log"
+	"os"
+	"runtime/trace"
 )
 
 func main() {
-	// Инициализация карточки пользователя
+
+	// Trace
+	f, err := os.Create("trace.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Print(err)
+		}
+	}()
+	err = trace.Start(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer trace.Stop()
+
 	user := card.Card{
 		Id: 1,
 		Owner: card.Owner{
@@ -19,9 +38,9 @@ func main() {
 		Number:       "5106212365738734",
 		Icon:         "https://www.mastercard.ru/content/dam/public/enterprise/resources/images/icons/favicon.ico",
 		Transactions: []card.Transaction{},
-	}
+	} // Инициализация карточки пользователя
 
-	err := user.MakeTransactions(50)
+	err = user.MakeTransactions(50)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -31,13 +50,13 @@ func main() {
 	fmt.Println(card.SumCategoryTransactions(user.Transactions))
 	fmt.Println("------------------------------------------")
 	fmt.Println("With mutex")
-	fmt.Println(card.SumCategoryTransactionsMutex(user.Transactions, 3))
+	fmt.Println(card.SumCategoryTransactionsMutex(user.Transactions, 10))
 	fmt.Println("------------------------------------------")
 	fmt.Println("With chan")
-	fmt.Println(card.SumCategoryTransactionsChan(user.Transactions, 3))
+	fmt.Println(card.SumCategoryTransactionsChan(user.Transactions, 10))
 	fmt.Println("------------------------------------------")
 	fmt.Println("With mutex without SumCategoryTransactions func")
-	fmt.Println(card.SumCategoryTransactionsMutexWithoutFunc(user.Transactions, 3))
+	fmt.Println(card.SumCategoryTransactionsMutexWithoutFunc(user.Transactions, 10))
 	fmt.Println("------------------------------------------")
 
 }
